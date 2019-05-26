d18547b7a486e3108887374b07b845a9968d53d6
naufalafif <nafualafif58@gmail.com>
2019-05-26 11:58:01 +0700

    initial commit change from gridsome starter blog : avatar, title, description, font & back button

---
+++ src/components/Logo.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="logo" @click="goBack()">
+	  <span class="logo__text">
+      &larr; Kembali
+	    <!-- &larr; {{ $static.metaData.siteName }} -->
+	  </span>
+	</div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  methods: {
+    goBack () {
+      window.history.back();
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.logo {
+	text-decoration: none;
+  color: var(--body-color)!important;
+  font-size: .9em;
+  cursor: pointer;
+  
+	&__image {
+		vertical-align: middle;
+		border-radius: 99px;
+		height: 40px;
+		width: 40px;
+		margin-right: .5em;
+	}	
+}
+</style>
